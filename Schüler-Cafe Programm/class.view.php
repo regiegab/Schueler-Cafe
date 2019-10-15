f1d8c6fb72786764f662e8cdf218018cf435df14
FelixIstHier <55846923+FelixIstHier@users.noreply.github.com>
2019-10-15 18:29:55 +0200

    edited MCV

---
+++ Schüler-Cafe Programm/class.view.php
@@ -0,0 +1,9 @@
+<?php
+class View{
+public function display($template){
+  include("")
+}
+}
+
+
+?>
--- Schüler-Cafe Programm/index.php
+++ Schüler-Cafe Programm/index.php
@@ -1,2 +1,8 @@
-test
-test2
+<?php
+include("class.control.php");
+include("class.view.php");
+include("class.model.php");
+
+$input = array_merge($_GET,$_POST);
+new Control($input);
+?>
