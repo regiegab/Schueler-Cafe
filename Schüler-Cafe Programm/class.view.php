dbb072f38e0bdd1378fb8d21fe2152b0d5658def
FelixIstHier <55846923+FelixIstHier@users.noreply.github.com>
2019-10-15 19:13:42 +0200

    MCV V1

--- Schüler-Cafe Programm/class.view.php
+++ Schüler-Cafe Programm/class.view.php
@@ -1,8 +1,24 @@
 <?php
 class View{
+
 public function display($template){
-  include("")
+
+  if(@include("Templates/".$template)){
+
+  }else{
+    echo "<br><br>invalid template!!!";
+    $errorMessage = "There was an unknown error with loading your web page!";
+    $this->displayError($errorMessage);
+  } // else Ende
+
+} // function Ende
+
+public function displayError($errorMessage){
+  echo "<br><br><br>".$errorMessage;
+  @include("Templates/errorWithLoadingPage.html");
 }
+
+
 }
 
 
