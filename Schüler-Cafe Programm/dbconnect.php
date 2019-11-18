9a3e3d819c83d1d6333ac25133e3e697a6dc2acb
regiegab <55846774+regiegab@users.noreply.github.com>
2019-11-18 16:40:28 +0100

    Einfügung von Login Dateien

--- Schüler-Cafe Programm/dbconnect.php
+++ Schüler-Cafe Programm/dbconnect.php
@@ -3,7 +3,7 @@ class Connect {
   public function __construct{}{
   }
 
-$db = mysqli_connect("localhost", "Benutzername", "Passwort", "Datenbankname");
+$db = mysqli_connect("localhost", "susocafe_mysql", "DiuSCDB%2019!", "susocafe");
 if(!$db)
 {
   exit("Verbindungsfehler: ".mysqli_connect_error());
