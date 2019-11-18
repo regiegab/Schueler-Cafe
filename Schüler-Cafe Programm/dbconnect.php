99ecf35c5437df73691770d68c676ba0f8bd4c97
regiegab <55846774+regiegab@users.noreply.github.com>
2019-11-18 16:34:52 +0100

    Klasse erstellt
    Klasse "Connect"

--- Schüler-Cafe Programm/dbconnect.php
+++ Schüler-Cafe Programm/dbconnect.php
@@ -1,7 +1,12 @@
 <?php
+class Connect {
+  public function __construct__{}{
+  }
+
 $db = mysqli_connect("localhost", "Benutzername", "Passwort", "Datenbankname");
 if(!$db)
 {
   exit("Verbindungsfehler: ".mysqli_connect_error());
+}
 }
 ?>
