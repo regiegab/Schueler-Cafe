2a28f9111535bbe32342038bdd91d1af789633b5
FelixIstHier <55846923+FelixIstHier@users.noreply.github.com>
2020-01-03 14:02:01 +0100

    Struktur uyum editieren der User im userTemplate geschaffen

--- Schüler-Cafe Programm/Templates/userInterface.php
+++ Schüler-Cafe Programm/Templates/userInterface.php
@@ -24,6 +24,18 @@
 
    <div id="userList" style="color:blue;display:none;">
      <h1>User List</h1>
+     <lo id="editField" style="display:none">
+       <h2>Edit</h2>:<br>
+       <!-- in the <a></a> the userId is saved -->
+       <form><a id="userId" style="display:none"></a>
+         <input type="text" id="edit_username" placeholder="username"> |
+         <input type="text" id="edit_password" placeholder="password"> |
+         <input type="number" id="edit_role" placeholder="role"> |
+         <input type="text" id="edit_description" placeholder="description"> |
+         <input type ="button" onclick="editUser()" value="Submit">
+         <input type ="button" onclick="hideEditField()" value="cancel">
+       </form>
+     </lo>
      <ul style="list-style-type:none">
        <?php
        if(isset($this->data['userList'])){
@@ -33,7 +45,7 @@
            $username = $value[1];
            $role = $value[2];
            $description = $value[3];
-           echo "<li name=\"".$name."\">".$username." | role: ".$role." | ".$description." | <button onclick=\"editUser(".$name.")\">edit</button> | <button onclick='deleteUser(".$name.",\"".$username."\")'>delete</button></li>";
+           echo "<li name=\"".$name."\">".$username." | role: ".$role." | ".$description." | <button onclick=\"openEdit(".$name.")\">edit</button> | <button onclick='deleteUser(".$name.",\"".$username."\")'>delete</button></li>";
            echo "<br>";
            // echo implode(" ",$value);
 
@@ -53,13 +65,32 @@
       document.getElementById('userList').style.display = "inline";
     }
 
-    function editUser(userId){
-      location.replace("http://susocafe.bplaced.net/index.php?action=open_userInterface&userInterface=editUser&user="+userId);
+    function openEdit(userId){
+      document.getElementById('userId').innerHTML = userId;
+      document.getElementById('editField').style.display = "inline";
+    }
+
+    function hideEditField(){
+      document.getElementById('editField').style.display = 'none';
+    }
+
+    function editUser(){
+      var userId = document.getElementById('userId').innerHTML;
+      // console.log(userId);
+      var username = document.getElementById('edit_username').value;
+      // console.log(username);
+      var password = document.getElementById('edit_password').value;
+      // console.log(password);
+      var role  = document.getElementById('edit_role').value;
+      // console.log(role);
+      var description = document.getElementById('edit_description').value;
+      // console.log(description);
+      location.replace("index.php?action=open_userInterface&userInterface=editUser&user="+userId+"&username="+username+"&password="+password+"&role="+role+"&description="+description);
     }
 
     function deleteUser(userId,username){
       if(confirm("Do you really want to delete the user \""+username+"\"?")){
-        location.replace("http://susocafe.bplaced.net/index.php?action=open_userInterface&userInterface=deleteUser&user="+userId);
+        location.replace("index.php?action=open_userInterface&userInterface=deleteUser&user="+userId);
       }
 
     }
